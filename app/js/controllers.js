23c455458e18b110fa52771501f68df9fc96f000
rapazz <moises.bravo@rapazz.cl>
2014-03-04 17:59:01 -0300

    ggg

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,11 +7,29 @@ angular.module('proyectosAppControllers', ['ui.bootstrap']).
 $http.get('json/proyectos.json').success(function(data) {
      $scope.arrProyectos = data;
  
-     $scope.openDialog=function(){
+     $scope.openDialog=function(tipo){
+var templateUrl =''
+var controlador = '' 
 
+switch (tipo) { 
+    case 'status': 
+        templateUrl = 'partials/modal_Avance.html'
+        controlador =  'modalActualizarEstado'
+         break 
+    case 'programacion': 
+          templateUrl = 'partials/modal_programacion.html'
+        controlador =  'modalProgramacion'
+         break 
+    case 'adjuntos': 
+         
+         break 
+    case 'ver': 
+         
+         break 
+    }
    var modalInstance = $modal.open({
-      templateUrl: 'partials/modal_Avance.html',
-      controller: 'modalActualizarEstado',
+      templateUrl: templateUrl,
+      controller: controlador,
       resolve: {
         items: function () {
           return $scope.items;
@@ -23,8 +41,20 @@ $http.get('json/proyectos.json').success(function(data) {
    });
 
   }])
-  .controller('modalActualizarEstado', ['$scope','$modalInstance',function($scope, $modalInstance) {
+  .controller('modalActualizarEstado', ['$scope','$modalInstance','$http',function($scope, $modalInstance,$http) {
 
+$http.get('json/saludProyecto.json').success(function(data) {
+$scope.saludProyecto= data 
+})
+
+
+$http.get('json/etapaProyecto.json').success(function(data) {
+$scope.etapaProyecto= data 
+})
+
+$http.get('json/estadoProyecto.json').success(function(data) {
+$scope.estadoProyecto= data 
+})
 
 
   $scope.ok = function () {
@@ -39,6 +69,26 @@ $http.get('json/proyectos.json').success(function(data) {
   .controller('mainController', ['$scope',function($scope) {
 
   	 $scope.message='HOLA AMOR'
+
+
+
+  }])
+   .controller('modalProgramacion', ['$scope','$modalInstance','$http',function($scope, $modalInstance,$http) {
+
+
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+
+  }])
+  .controller('mainController', ['$scope',function($scope) {
+
+     $scope.message='HOLA AMOR'
 
 
 
